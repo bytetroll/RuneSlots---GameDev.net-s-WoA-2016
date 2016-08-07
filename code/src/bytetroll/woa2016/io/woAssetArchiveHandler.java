784544a3a4eb047d4ca6c34a66db81b40630b79f
Nathan Young <nyoung@executesoftware.com>
2016-08-07 04:31:11 -0400

    Started a load all routine for the asset archive handler.

--- code/src/bytetroll/woa2016/io/woAssetArchiveHandler.java
+++ code/src/bytetroll/woa2016/io/woAssetArchiveHandler.java
@@ -2,12 +2,42 @@ package bytetroll.woa2016.io;
 
 import bytetroll.woa2016.runtime.woRuntime;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
 public class woAssetArchiveHandler {
     public static void RegisterAssetDirectory(String path) {
         assetDir = path;
+    }
+
+    // This is a super ghetto shortcut to speed up development time.
+    // This will work for us since we won't have many assets, but
+    // caching all asset archive files into memory at once is a shitty
+    // idea!
+
+    //@Todo: See here also for the "finish the asset handler" todo.  Also,
+    //       remember to remove this comment ;)
+    public static void CacheAllInAssetDirectory() {
+        class woAssetDirectoryWalker extends SimpleFileVisitor<Path> {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException except) {
+
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException except) {
+
+            }
+        }
     }
 
     public static void LoadArchive(String path) {
