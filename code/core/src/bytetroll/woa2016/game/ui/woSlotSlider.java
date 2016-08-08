5acb22f5ec39b474590a0aed4e1b84443a94e282
Nathan Young <nyoung@executesoftware.com>
2016-08-08 03:33:06 -0400

    asdf

---
+++ code/core/src/bytetroll/woa2016/game/ui/woSlotSlider.java
@@ -0,0 +1,17 @@
+package bytetroll.woa2016.game.ui;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class woSlotSlider extends Image {
+
+    private String currentEffect = null;
+    private int currentRow = 0;
+
+    private int originalX = 0;
+    private int originalY = 0;
+    private float originalWidth = 0.0f;
+    private float originalHeight = 0.0f;
+
+    private Animation anim = null;
+}
