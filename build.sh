0af48db002ab5352ef5274f634217dae07285e83
Nathan Young <nyoung@executesoftware.com>
2016-08-08 01:57:22 -0400

    Fixed build system.

---
+++ build.sh
@@ -0,0 +1,13 @@
+cd "code"
+
+gradle desktop:dist
+
+cd "../"
+
+perl ./chain/pak.pl /Volumes/exec/work/projects/woa2016/pack /Volumes/exec/work/projects/woa2016/build/woa2016
+perl ./chain/pak.pl /Volumes/exec/work/projects/woa2016/code/desktop/build/libs /Volumes/exec/work/projects/woa2016/build
+
+echo Y | rm -r tmp
+echo Y | rm -r dependency-cache
+echo Y | rm -r classes
+echo Y | rm -r libs
