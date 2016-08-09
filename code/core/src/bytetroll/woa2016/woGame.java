e02afbae9dd4fcbcb66a1e5ae28cb4512cc89f00
Nathan Young <nyoung@executesoftware.com>
2016-08-09 10:29:17 -0400

    Backup of running project before implementing truetype font rendering for the UI.

--- code/core/src/bytetroll/woa2016/woGame.java
+++ code/core/src/bytetroll/woa2016/woGame.java
@@ -22,7 +22,7 @@ public class woGame extends ApplicationAdapter {
 
         final woAsset font = woAssetHandler.Find("default.fnt");
         final woAsset tex = woAssetHandler.Find("bg_btn5.png");
-        scene.SpawnActor(new woLabel("Whoop!", font, tex));
+        scene.SpawnActor(new woLabel("Hi!", font, tex));
 	}
 
 	@Override
