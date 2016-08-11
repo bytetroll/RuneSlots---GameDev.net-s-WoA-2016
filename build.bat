86a09f7b83c2ab410ce8167357a8d10c9d933929
Nathan Young <bytetroll@hotmail.com>
2016-08-11 04:04:45 +0100

    Added a Windows build file.

---
+++ build.bat
@@ -0,0 +1,23 @@
+echo off
+
+cd %~dp0
+
+rmdir /S /Q "build"
+
+cd "code"
+
+call gradle desktop:dist
+
+cd "../"
+
+call perl chain/pak.pl E:/Work/Projects/woa2016/pack E:/Work/Projects/woa2016/build/woa2016
+call perl chain/pak.pl E:/Work/Projects/woa2016/code/desktop/build/libs E:/Work/Projects/woa2016/build
+
+rmdir /S /Q "tmp"
+rmdir /S /Q "dependency-cache"
+rmdir /S /Q "classes"
+rmdir /S /Q "libs"
+
+cd "build"
+
+ren "desktop-1.0.jar" "woa2016.jar"
